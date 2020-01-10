09049549f0a7ee50317e94d917173a02814ae9ce
Edilmar Santos Nascimento <edilmar.nascimento@kantaribopemedia.com>
2020-01-10 17:31:42 -0200

    Created email-validator and respectives tests, also added  covered folder into .gitignore

--- src/utils/helpers/email-validator.spec.js
+++ src/utils/helpers/email-validator.spec.js
@@ -1,16 +1,23 @@
 const EmailValidator = require('./email-validator')
 const validator = require('validator')
+const MissingParamError = require('../errors/missing-param-error')
 
 const makeSut = () => {
   return new EmailValidator()
 }
 
-describe('Email Validtor', () => {
+describe('Email Validator', () => {
   test('should return true when provided a valid email', () => {
     const sut = makeSut()
 
     const isValidValid = sut.isValid('valid_email@email.com')
     expect(isValidValid).toBe(true)
+  })
+
+  test('should throws MissinParamError when email not provided', () => {
+    const sut = makeSut()
+
+    expect(sut.isValid).toThrow(new MissingParamError('email'))
   })
 
   test('should return false when provided a invalid email', () => {
