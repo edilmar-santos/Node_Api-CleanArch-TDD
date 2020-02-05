1260993dc7bcc17e0469f39f822181e20af3366a
Edilmar Santos Nascimento <edilmar.nascimento@kantaribopemedia.com>
2020-02-05 09:06:45 -0200

    Refactored mongo-helper

--- src/infra/helpers/mongo-helper.spec.js
+++ src/infra/helpers/mongo-helper.spec.js
@@ -1,13 +1,20 @@
-const MongoHelper = require('./mongo-helper')
+const sut = require('./mongo-helper')
 
 describe('Mongo helper', () => {
-  test('should connect when call getDb() and there is no connection', async () => {
-    const sut = MongoHelper
+  beforeAll(async () => {
     await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.closeConnection()
+  })
+
+  test('should connect when call getDb() and there is no connection', async () => {
     expect(sut.db).toBeTruthy()
     await sut.closeConnection()
     expect(sut.db).toBeFalsy()
     const db = await sut.getDb()
     expect(db).toBeTruthy()
+    await sut.closeConnection()
   })
 })
